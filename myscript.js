e3544ebaab77fd89654abd2f0a602e5e934c37a3
WalterFrongia <walter.frongia@libero.it>
2021-09-13 17:36:10 +0200

    Aggiunta funzione per stampare su html

--- myscript.js
+++ myscript.js
@@ -111,14 +111,49 @@ let icons =
 
 let contenitoreIcons = document.getElementById("my-container-icons")  //salvo in una variabile la stampa per html 
 
-icons.forEach((element) => {   //utilizzo il foEach per ciclare ogni elemento dell'oggetto nell'array
-    console.log(element);   
-                                    // questo serve per stampare codice html nel file JS, 
-                                    // la sintassi dollaro e parentesi graffe serve per aggiungere variabili e oggetti con valore specifico tramite 
-                                    //il punto
-                                    // += (serve per aggiungere ogni elemento)  
-    contenitoreIcons.innerHTML +=  `<li class="my-icon"> 
-                                        <i class="${element.family} ${element.prefix}${element.name}"></i>
-                                        <p>${element.name}</p>
-                                    </li>`
+stampaHtml(icons, contenitoreIcons); //così richiamo la funzione per la stampa e do gli argomenti che a me interessano (array di oggetti "icons")
+                                     // e contenitoreIcons per html.
+
+
+//CREAREUN ARRAY PER TUTTI I TIPI IN MODO DINAMICO
+// CREO UN ARRAY VUOTO DI TIPI 
+// ARRAY FATTO SU ICONS 
+// ALL'INTERNO DEL FOREACH DEVO PRENDERE IL TYPE DI ICONA E AGGIUNGERE NELL'ARRAY VUOTO SE NON E' PRESENTE.
+
+icons.forEach((element) => {
+
+    let arrayIcons = [];
+    
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//FUNZIONE CHE SERVE PER LA STAMPA HTML 
+function stampaHtml(array, contenitore){
+    array.forEach((element) => {   //utilizzo il foEach per ciclare ogni oggetto nell'array   
+
+                                        // questo serve per stampare codice html nel file JS, 
+                                        // la sintassi dollaro e parentesi graffe serve per aggiungere variabili e oggetti con valore specifico tramite 
+                                        //il punto
+                                        // += (serve per aggiungere ogni elemento mantenendo gli elementi già stampati)  
+        contenitore.innerHTML +=  
+            `<li class="my-icon"> 
+                <i class="${element.family} ${element.prefix}${element.name}"></i>
+                <p>${element.name}</p>
+            </li>`
+    });
+}
